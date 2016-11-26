456435953564bf14489912238c985cf2014a5607
19mariusz95 <19mariusz95@gmail.com>
2016-11-26 19:32:06 +0100

    fitness and energy fields

--- src/main/java/pl/edu/agh/to/agent/Agent.java
+++ src/main/java/pl/edu/agh/to/agent/Agent.java
@@ -5,12 +5,25 @@ import pl.edu.agh.to.genotype.Genotype;
 public class Agent {
 
     private final Genotype genotype;
+    private final double fitness;
+    private final double energy;
 
-    public Agent(Genotype genotype) {
+
+    public Agent(Genotype genotype, double fitness, double energy) {
         this.genotype = genotype;
+        this.fitness = fitness;
+        this.energy = energy;
     }
 
     public Genotype getGenotype() {
         return genotype;
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public double getEnergy() {
+        return energy;
     }
 }
