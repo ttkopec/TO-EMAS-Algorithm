a8e7ab81410ac53cc67e11d38da78aca932d914b
Wookie <lukasz.lampart.priv@gmail.com>
2016-12-04 16:29:19 +0100

    Mock core version, showing basic application concept

---
+++ src/main/java/pl/edu/agh/to/IslandUtils.scala
@@ -0,0 +1,25 @@
+package pl.edu.agh.to
+
+import scala.collection.mutable.{Set => MSet}
+import scala.language.implicitConversions
+
+object IslandUtils {
+
+  class NamingUtils[E](private val data: Seq[E]) extends AnyVal {
+
+    def getCrossoverSeq(combiner: (E, E) => Option[E]): Seq[E] = {
+      val groupedAgents = data.grouped(2)
+      groupedAgents.flatMap {
+        case Seq(a, b) =>
+          combiner(a, b)
+        case Seq(a) =>
+          Iterator.single(a)
+      }.toSeq
+    }
+
+  }
+
+  implicit def setNamingUtils[Agent](set: MSet[Agent]): NamingUtils[Agent] =
+    new NamingUtils(set.toSeq)
+
+}
