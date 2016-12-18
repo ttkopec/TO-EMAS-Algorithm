df0b75267a0e309be671b6fc079e31e78aee7ed7
Wookie <lukasz.lampart.priv@gmail.com>
2016-12-18 17:59:59 +0100

    Mocked tests

--- src/test/src/pl/edu/agh/to/operators/OperatorTest.java
+++ src/test/src/pl/edu/agh/to/operators/OperatorTest.java
@@ -2,21 +2,44 @@ package pl.edu.agh.to.operators;
 
 import org.junit.Test;
 import pl.edu.agh.to.agent.Agent;
+import pl.edu.agh.to.agent.AgentConfig;
+import pl.edu.agh.to.core.util.OperatorsConfig;
+import pl.edu.agh.to.core.util.TypedOperatorsConfig;
 import pl.edu.agh.to.genotype.Genotype;
+import scala.Option;
+import scala.reflect.internal.util.Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by tkopec on 04.12.16.
- */
 public class OperatorTest {
-    private Agent agent = new Agent(null, 0, new Operator());
+    private AgentConfig config = new AgentConfig(50, 200, 0);
+    private OperatorsConfig operatorsConfig = new OperatorsConfig() {
 
-    @Test(expected = NumberFormatException.class)
-    public void testEvaluation() {
-        agent.setGenotype(new Genotype("numberFormatException"));
-        agent.getFitness();
-    }
+        @Override
+        public Operator crossOverOperator() {
+            return new CrossOverOperator();
+        }
+
+        @Override
+        public Operator mutationOperator() {
+            return new MutationOperator();
+        }
+
+        @Override
+        public Operator selectionOperator() {
+            return new SelectionOperator();
+        }
+
+        @Override
+        public Operator evaluationOperator() {
+            return new EvaluationOperator();
+        }
+    };
+    private Agent agent = new Agent(null, 0, config);
 
     @Test
     public void testSelection(){
@@ -29,17 +52,29 @@ public class OperatorTest {
 
     @Test
     public void testMutation(){
-        agent.setGenotype(new Genotype("1230"));
-        assertNotEquals(agent, agent.mutate(10));
-
-
-
+        Random random = new Random();
+        List<Double> genotypeExample = new ArrayList<>(10);
+        for (int i=0; i<10; ++i) {
+            genotypeExample.add(random.nextDouble());
+        }
+        agent.setGenotype(new Genotype(genotypeExample));
+        assertNotEquals(agent, operatorsConfig.mutationOperator().execute(agent));
     }
 
     @Test
     public void testCrossing(){
-        agent.setGenotype(new Genotype("123445665432432524566857634"));
-        agent.setEnergy(50);
-        assertNotEquals(agent, agent.cross(new Agent(new Genotype("0984324"), 10, null)));
+        Random random = new Random();
+        List<Double> genotypeExample = new ArrayList<>(10);
+        List<Double> secondGenotypeExample = new ArrayList<>(10);
+        for (int i=0; i<10; ++i) {
+            genotypeExample.add(random.nextDouble());
+            secondGenotypeExample.add(random.nextDouble());
+        }
+        agent.setGenotype(new Genotype(genotypeExample));
+        agent.setEnergy(100);
+
+
+        Agent secondAgent = new Agent(new Genotype(secondGenotypeExample), 100, config);
+        assertNotEquals(agent, operatorsConfig.crossOverOperator().execute(agent, secondAgent));
     }
 }
